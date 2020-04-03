9d752d756831b6f823a33c553938967ac77d3226
lise houblon <lisehoublon@gmail.com>
2020-04-03 11:58:02 +0200

    implémentation de la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -4,5 +4,39 @@
  *
  */
 public class Utilisateur {
+	
+	/*_______________________________props________________________________*/
+	private int id;
+	private String nom;
+	
 
+	/*_______________________________ctors________________________________*/
+	public Utilisateur() {
+	
+	}
+
+
+	
+	
+	/*________________________getters/sett________________________________*/
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
 }// end class
